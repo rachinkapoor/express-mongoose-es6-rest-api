e3fc9c950d9ee8c00c49555f20e88b5eac6731b5
Preshita Shirke <preshita@ost.com>
2019-03-26 17:45:07 +0530

    code changes

--- server/token/token.controller.js
+++ server/token/token.controller.js
@@ -7,21 +7,17 @@ const ostSdk = require("../helpers/ostSdk");
 function get(req, res) {
   const tokensService = ostSdk.services.tokens,
     pricePointService = ostSdk.services.price_points;
+
+  var chainId, token;
   tokensService
     .get({})
     .then(function(response) {
-      var chainId = response.data["token"]["auxiliary_chains"][0]["chain_id"],
-        token = response.data["token"];
-      pricePointService
-        .get({ chain_id: chainId })
-        .then(function(response) {
-          res.send(
-            JSON.stringify({ token: token, price_points: response.data })
-          );
-        })
-        .catch(function(err) {
-          res.send(JSON.stringify(err));
-        });
+      chainId = response.data["token"]["auxiliary_chains"][0]["chain_id"];
+      token = response.data["token"];
+      return pricePointService.get({ chain_id: chainId });
+    })
+    .then(function(response) {
+      res.send(JSON.stringify({ token: token, price_points: response.data }));
     })
     .catch(function(err) {
       res.send(JSON.stringify(err));
